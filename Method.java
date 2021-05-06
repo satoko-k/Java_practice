0cff558c7afc0fe6a630176398c3e1cb9123a289
“satoko-k” <satoko617k@gmil.com>
2021-05-06 16:08:23 +0900

    3rd

---
+++ Method.java
@@ -0,0 +1,29 @@
+class Method {
+
+        public static void main(String[] args) {
+         
+          // String name = fullName("鈴木","花子");  
+          // printData(name, 27);
+          printData(fullName("鈴木","花子"),27);
+
+          printData(fullName("John", "Christopher", "Smith"), 65);
+          
+        }
+      
+        public static void printData(String name, int age) {
+          System.out.println("私の名前は" + name + "です");
+          System.out.println("年齢は" + age + "歳です");
+        }
+      
+        // fullNameメソッドを定義
+        public static String fullName(String firstName,String lastName){
+          return firstName + " " + lastName;
+        }
+        // 同名のメソッド　fullNameメソッド　オーバーロード
+        public static String fullName(String firstName, String middleName, String lastName){
+          return firstName + " " + middleName + " " + lastName;
+        }
+        
+}
+      
+    
---
+++ fortest.java
@@ -0,0 +1,27 @@
+class Fortest {
+    public static void main(String[] args){
+                
+        // String[] names = {"佐藤", "鈴木", "田中","高橋"};
+        
+        // for(int i=0; i<names.length; i++){
+        // System.out.println("私の名前は"+ names[i] +"です");
+        // }
+        
+        
+        // 拡張for文
+
+        String[] names = {"佐藤", "鈴木", "田中","高橋"};
+
+        for(String name : names){
+          System.out.println("私の名前は"+name + "です");
+        }
+
+
+
+
+
+
+
+    }
+    
+}
